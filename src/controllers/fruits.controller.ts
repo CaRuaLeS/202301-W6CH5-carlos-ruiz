3bdfc83a59c62e7fc60fa08f0aa9567b690597d4
CarlosRuiz <carua97@gmail.com>
2023-02-25 21:09:36 +0100

    Add controller file

--- src/controllers/fruits.controller.ts
+++ src/controllers/fruits.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { FruitsRepoStructure } from '../repository/fruits.file.repo';
+import { FruitsRepoStructure } from '../repository/fruits.file.repo.js';
 
 export class Fruitscontroller {
   // eslint-disable-next-line no-useless-constructor, no-unused-vars
@@ -9,8 +9,31 @@ export class Fruitscontroller {
     this.repo.read().then((data) => resp.json(data));
   }
 
+  get(req: Request, resp: Response) {
+    const dataId = Number(req.params.id);
+    this.repo
+      .readById(dataId)
+      .then((data) =>
+        data === undefined
+          ? resp.status(404).send('No id elements')
+          : resp.json(data)
+      );
+  }
+
+  async update(req: Request, resp: Response) {
+    const data = req.body;
+    this.repo.update(data).then(() => {
+      resp.status(200).send(data);
+    });
+  }
+
   create(req: Request, _resp: Response) {
     console.log(req.body);
     this.repo.write(req.body).then((data) => console.log(data));
+  }
+
+  async delete(req: Request, resp: Response) {
+    await this.repo.delete(Number(req.params.id));
+    resp.status(200).send(`Delete id: ${req.params.id}`);
   }
 }
