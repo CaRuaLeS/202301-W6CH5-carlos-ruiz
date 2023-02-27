d2d5f66ffe85398743b9314c55a524e6e2a8dba8
CarlosRuiz <carua97@gmail.com>
2023-02-27 18:18:39 +0100

    Modified distribution and variables controller and repo

--- src/controllers/fruits.controller.ts
+++ src/controllers/fruits.controller.ts
@@ -1,39 +1,64 @@
-import { Request, Response } from 'express';
-import { FruitsRepoStructure } from '../repository/fruits.file.repo.js';
+import { NextFunction, Request, Response } from 'express';
+import { Fruit } from '../entites/fruit';
+import { FruitsRepoStructure } from '../repository/repo.interface';
 
 export class Fruitscontroller {
   // eslint-disable-next-line no-useless-constructor, no-unused-vars
-  constructor(public repo: FruitsRepoStructure) {}
+  constructor(public repo: FruitsRepoStructure<Fruit>) {}
 
-  getAll(_req: Request, resp: Response) {
-    this.repo.read().then((data) => resp.json(data));
+  async getAll(_req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.query();
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  get(req: Request, resp: Response) {
-    const dataId = Number(req.params.id);
-    this.repo
-      .readById(dataId)
-      .then((data) =>
-        data === undefined
-          ? resp.status(404).send('No id elements')
-          : resp.json(data)
-      );
+  async get(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.queryId(req.params.id);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async update(req: Request, resp: Response) {
-    const data = req.body;
-    this.repo.update(data).then(() => {
-      resp.status(200).send(data);
-    });
+  async post(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.create(req.body);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  create(req: Request, _resp: Response) {
-    console.log(req.body);
-    this.repo.write(req.body).then((data) => console.log(data));
+  async patch(req: Request, resp: Response, next: NextFunction) {
+    try {
+      req.body.id = req.params.id ? req.params.id : req.body.id;
+      const data = await this.repo.update(req.body);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async delete(req: Request, resp: Response) {
-    await this.repo.delete(Number(req.params.id));
-    resp.status(200).send(`Delete id: ${req.params.id}`);
+  async delete(req: Request, resp: Response, next: NextFunction) {
+    try {
+      this.repo.delete(req.params.id);
+      resp.json({
+        results: [],
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- src/repository/fruits.file.repo.ts
+++ src/repository/fruits.file.repo.ts
@@ -1,60 +1,60 @@
-/* eslint-disable no-unused-vars */
 import fs from 'fs/promises';
+import { Fruit } from '../entites/fruit';
+import { FruitsRepoStructure } from './repo.interface';
 
 const file = 'data/fruits.json';
 
-export type Fruit = {
-  id: number;
-  name: string;
-  color: string;
-  weigth: number;
-};
-
-export interface FruitsRepoStructure {
-  read(): Promise<Fruit[]>;
-  readById(id: Fruit['id']): Promise<Fruit>;
-  write(info: Fruit): Promise<void>;
-  update(info: Fruit): Promise<void>;
-  delete(id: Fruit['id']): Promise<void>;
-}
-
-export class FruitsFileRepo implements FruitsRepoStructure {
-  read() {
-    return fs
-      .readFile(file, { encoding: 'utf-8' })
-      .then((data) => JSON.parse(data) as Fruit[]);
+export class FruitsFileRepo implements FruitsRepoStructure<Fruit> {
+  async query(): Promise<Fruit[]> {
+    const initialData: string = await fs.readFile(file, { encoding: 'utf-8' });
+    return JSON.parse(initialData);
   }
 
-  async readById(id: Fruit['id']) {
-    const data = await fs.readFile(file, { encoding: 'utf-8' }).then((data) => {
-      const dataParsed: Fruit[] = JSON.parse(data);
-      const finalData = dataParsed.filter((item) => item.id === id)[0];
-      return finalData;
+  async queryId(id: string): Promise<Fruit> {
+    const initialData: string = await fs.readFile(file, { encoding: 'utf-8' });
+    const data: Fruit[] = JSON.parse(initialData);
+    const finalData = data.find((item) => item.id === id);
+    if (!finalData) throw new Error('Id not found');
+    return finalData;
+  }
+
+  async create(info: Partial<Fruit>): Promise<Fruit> {
+    // Future if (!validateInfo(info)) throw new Error('Not valid data');
+    const initialData: string = await fs.readFile(file, { encoding: 'utf-8' });
+    const data: Fruit[] = JSON.parse(initialData);
+    info.id = String(Math.floor(Math.random() * 1000_000));
+    const finalData = [...data, info];
+    await fs.writeFile(file, JSON.stringify(finalData), 'utf-8');
+    return info as Fruit;
+  }
+
+  async update(info: Partial<Fruit>): Promise<Fruit> {
+    if (!info.id) throw new Error('Not valid data');
+    const initialData: string = await fs.readFile(file, { encoding: 'utf-8' });
+    const data: Fruit[] = JSON.parse(initialData);
+    let updatedItem: Fruit = {} as Fruit;
+    const finalData = data.map((item) => {
+      if (item.id === info.id) {
+        updatedItem = { ...item, ...info };
+        return updatedItem;
+      }
+
+      return item;
     });
-    return data;
+
+    if (!updatedItem.id) throw new Error('Id not found');
+    await fs.writeFile(file, JSON.stringify(finalData), 'utf-8');
+    return updatedItem as Fruit;
   }
 
-  async write(info: Fruit) {
-    const data = await fs.readFile(file, { encoding: 'utf-8' });
-    const dataParsed: Fruit[] = JSON.parse(data);
-    const finalData = JSON.stringify([...dataParsed, info]);
-    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
-  }
-
-  async update(info: Fruit) {
-    const data = await fs.readFile(file, { encoding: 'utf-8' });
-    const dataParsed: Fruit[] = JSON.parse(data);
-    const finalData = JSON.stringify(
-      dataParsed.map((item) => (item.id === info.id ? info : item))
-    );
-    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
-  }
-
-  async delete(id: Fruit['id']) {
-    const data = await fs.readFile(file, { encoding: 'utf-8' });
-    const dataParsed: Fruit[] = JSON.parse(data);
-    const dataFiltered = dataParsed.filter((item) => item.id !== id);
-    const finalData = JSON.stringify(dataFiltered);
-    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
+  async delete(id: string): Promise<void> {
+    const initialData: string = await fs.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const data: Fruit[] = JSON.parse(initialData);
+    const index = data.findIndex((item) => item.id === id);
+    if (index < 0) throw new Error('Id not found');
+    data.slice(index, 1);
+    await fs.writeFile(file, JSON.stringify(data), 'utf-8');
   }
 }
