e30cae71519dddf47db3cc17b4861331deec6c0d
CarlosRuiz <carua97@gmail.com>
2023-02-28 21:21:03 +0100

    Add tests mongo repo and controller

---
+++ src/repository/fruit.mongoose.repo.test.ts
@@ -0,0 +1,108 @@
+import { FruitModel } from './fruit.mongo.model';
+import { FruitMongooseRepo } from './fruit.mongoose.repo';
+
+jest.mock('./fruit.mongo.model');
+
+describe('Given FruitMongoRepo', () => {
+  const repo = new FruitMongooseRepo();
+
+  describe('When i use query', () => {
+    test('Then should return the data', async () => {
+      (FruitModel.find as jest.Mock).mockResolvedValue([]);
+      const result = await repo.query();
+
+      expect(FruitModel.find).toHaveBeenCalled();
+      expect(result).toEqual([]);
+    });
+  });
+  describe('When you use queryId()', () => {
+    test('Then it should return the data', async () => {
+      // Arrange
+      (FruitModel.findById as jest.Mock).mockResolvedValue({ id: '1' });
+      // Act
+      const id = '1';
+      const result = await repo.queryId(id);
+      // Assert
+      expect(FruitModel.findById).toHaveBeenCalled();
+      expect(result).toEqual({ id: '1' });
+    });
+    test('Then should throw an error', () => {
+      // Arrange
+      (FruitModel.findById as jest.Mock).mockResolvedValue(undefined);
+      // Act
+      const id = '1';
+      // Assert
+      expect(async () => repo.queryId(id)).rejects.toThrow();
+      expect(FruitModel.findById).toHaveBeenCalled();
+    });
+  });
+  describe('When you use create()', () => {
+    test('Then it should return the data', async () => {
+      // Arrange
+      const mockNewItem = { id: '2' };
+      (FruitModel.create as jest.Mock).mockResolvedValue({ id: '1' });
+      // Act
+      const result = await repo.create(mockNewItem);
+      // Assert
+      const resultId = result.id;
+      expect(FruitModel.create).toHaveBeenCalled();
+      expect(result).toEqual({ id: resultId });
+    });
+    test('Then should throw an error', () => {
+      // Arrange
+      (FruitModel.create as jest.Mock).mockResolvedValue(undefined);
+      // Act
+      const mockNewItem = { id: '1', test: 3 };
+
+      // Assert
+      expect(async () => repo.create(mockNewItem)).rejects.toThrow();
+      expect(FruitModel.create).toHaveBeenCalled();
+    });
+  });
+  describe('When you use update()', () => {
+    test('Then it should return the data', async () => {
+      // Arrange
+      const mockNewItem = { id: '1', test: 3 };
+      (FruitModel.findByIdAndUpdate as jest.Mock).mockResolvedValue({
+        id: '1',
+        test: 3,
+      });
+      // Act
+      const result = await repo.update(mockNewItem);
+      // Assert
+      expect(FruitModel.findByIdAndUpdate).toHaveBeenCalled();
+      expect(result).toEqual({ id: '1', test: 3 });
+    });
+    test('Then should throw an error', () => {
+      // Arrange
+      (FruitModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(undefined);
+      // Act
+      const mockNewItem = { id: '1', test: 3 };
+
+      // Assert
+      expect(async () => repo.update(mockNewItem)).rejects.toThrow();
+      expect(FruitModel.findByIdAndUpdate).toHaveBeenCalled();
+    });
+  });
+  describe('When you use delete()', () => {
+    test('Then it should return the data', async () => {
+      // Arrange
+      (FruitModel.findByIdAndDelete as jest.Mock).mockResolvedValue(
+        '[{"id": "1", "test": "3"}]'
+      );
+      // Act
+      const result = await repo.delete('1');
+      // Assert
+      expect(result).toBeUndefined();
+    });
+    test('Then should throw an error', () => {
+      // Arrange
+      (FruitModel.findByIdAndDelete as jest.Mock).mockResolvedValue(undefined);
+      // Act
+
+      // Assert
+      expect(async () => repo.delete('1')).rejects.toThrow();
+      expect(FruitModel.findByIdAndDelete).toHaveBeenCalled();
+    });
+  });
+});
