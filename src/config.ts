c35b15b1903efc8880b7961696dfaab1716ca1f9
CarlosRuiz <carua97@gmail.com>
2023-03-01 17:45:54 +0100

    Add app and config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
+  // Estos elementos son de mongoDB
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   cluster: process.env.DB_CLUSTER,
   dbName: process.env.DB_NAME,
+  // Esto es para JWT
+  jwtSecret: process.env.SECRET,
 };
